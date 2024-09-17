7dfaeaf7f83f80be6d22dbb9e579ce007a4b4771
gycsaba <gycsaba@SZT9-24>
2024-09-17 15:12:14 +0200

    Valódi osztók önmaga a szám és az egy

--- SzamelmeletProjekt/Szamelmelet.cs
+++ SzamelmeletProjekt/Szamelmelet.cs
@@ -7,7 +7,12 @@ namespace SzamelmeletProjekt
         // Statikus osztályban statikus metódusokat lehet készíteni
         public static List<int> GetOsztok(int szam)
         {
-            return new List<int>();
+            // Pl. 6 osztói 1,2,3,
+            List<int> osztok = new List<int>();
+            osztok.Add(szam);
+            osztok.Add(1);
+
+            return osztok;
         }
     }
 }
